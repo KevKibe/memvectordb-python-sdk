2db6eb146070fce6a2e310186afdbbcbce5efdd5
KevKibe <kchegz234@gmail.com>
2024-05-17 19:24:33 +0300

    chore: add tests and test workflow

---
+++ tests/memvectordb_test.py
@@ -0,0 +1,15 @@
+import unittest
+from memvectordb.collection import MemVectorDB
+
+class TestMemVectorDB(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = MemVectorDB(base_url="http://0.0.0.0:8000")
+        self.collection_name = "test_collection_name"
+        return super().setUp()
+    
+    def test_create_collection_method(self):
+        dimension = 3
+        distance = "Cosine" 
+        collection = self.client.create_collection(self.collection_name, dimension, distance)
+        expected_string = f"Successfully created collection: Collection {{ dimension: {dimension}, distance: {distance}, embeddings: [] }}"
+        self.assertIn(expected_string, collection, f"Expected string not found in collection: {expected_string}")
